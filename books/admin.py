a6461835bcbd6add5e9f6957cb9adc090ea109f6
ShesAmbi <shesambi@gmail.com>
2025-09-18 11:19:41 +0330

    Delete books directory

--- books/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BookCard
-
-@admin.register(BookCard)
-class BookCardAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "order", "created_at")
-    search_fields = ("title", "slug")
-    ordering = ("order", "-created_at")
--- books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "books"
-
-urlpatterns = [
-    path("cards/", views.cards_list, name="cards-list"),
-    path("cards/<slug:slug>/", views.card_detail, name="cards-detail"),
-]
